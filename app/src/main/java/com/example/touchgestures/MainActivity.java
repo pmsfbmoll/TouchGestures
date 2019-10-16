315fcfd3dc76d3c42d38b3b3533a0eb84b9d85d5
Alumnat <Alumnat@PROTEUS07.sinf2003srvr2.local>
2019-10-16 19:05:45 +0200

    Initial commit

---
+++ app/src/main/java/com/example/touchgestures/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.touchgestures;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.MotionEventCompat;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+
+public class MainActivity extends AppCompatActivity {
+    public static String DEBUG_TAG = "Gesture debug";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        int action = MotionEventCompat.getActionMasked(event);
+
+        switch (action) {
+            case (MotionEvent.ACTION_DOWN):
+                Log.d(DEBUG_TAG, "Action was DOWN");
+                return true;
+            case (MotionEvent.ACTION_MOVE):
+                Log.d(DEBUG_TAG, "Action was MOVE");
+                return true;
+            case (MotionEvent.ACTION_UP):
+                Log.d(DEBUG_TAG, "Action was UP");
+                return true;
+            case (MotionEvent.ACTION_CANCEL):
+                Log.d(DEBUG_TAG, "Action was CANCEL");
+                return true;
+            case (MotionEvent.ACTION_OUTSIDE):
+                Log.d(DEBUG_TAG, "Movement occurred outside bounds " +
+                        "of current screen element");
+                return true;
+            default:
+                return super.onTouchEvent(event);
+        }
+
+    }
+}
